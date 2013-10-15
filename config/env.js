e8c8c4e50750a36907d378ed7d1bfa412782438f
Yonatan Kogan <yonatan@romotive.com>
2013-10-15 02:43:52 -0700

    Allow for TUMBLR envars to be set w/o file

--- config/env.js
+++ config/env.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 function set(name, value) {
   if (!(name in process.env)) {
     process.env[name] = value;
@@ -12,6 +14,9 @@ set('NODE_ENV', 'development');
 
 // The port for the HTTP server to listen on
 set('PORT', 8000);
-var tumblr = require('./tumblr');
-set('TUMBLR_KEY', tumblr.TUMBLR_KEY);
-set('TUMBLR_SECRET', tumblr.TUMBLR_SECRET);
+
+if (fs.existsSync(__dirname + '/tumblr.js')) {
+	var tumblr = require('./tumblr');
+	set('TUMBLR_KEY', tumblr.TUMBLR_KEY);
+	set('TUMBLR_SECRET', tumblr.TUMBLR_SECRET);
+}
